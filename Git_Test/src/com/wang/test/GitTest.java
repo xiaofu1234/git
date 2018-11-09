934c2c62561040f5afd0701cdfafc60690989761
wangxiaofu <xiaofu@qq.com>
2018-11-09 09:40:30 +0800

    提交本地工程到git

---
+++ Git_Test/src/com/wang/test/GitTest.java
@@ -0,0 +1,23 @@
+/**
+ * @Description: java类作用描述
+ * @Author: wnn
+ * @CreateDate: 2018/11/9 0009$ 9:30$
+ * @Version: 1.0
+ */
+
+
+package com.wang.test;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello ya git!");
+        System.out.println("hello ya git!");
+        System.out.println("hello ya git!");
+        System.out.println("hello ya git!");
+        System.out.println("hello ya git!");
+        System.out.println("hello ya git!");
+    }
+
+
+}
